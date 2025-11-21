f520004295a7a8c24871cb861694f6b1bdc45a1d
Tinh <tinhb2207571@student.ctu.edu.vn>
2025-11-22 03:12:31 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,14 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
-
+    
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
+   
     {
         path: "/:pathMatch(.*)*",
         name: "notfound",
